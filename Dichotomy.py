ed1965e2edbb6df07baeda0460a4b05bfae1ac10
prometeyqwe <kirilllov.evgeny@mail.ru>
2018-08-26 22:46:20 +0300

    create file

---
+++ Dichotomy.py
@@ -0,0 +1,20 @@
+import math
+
+elps = math.exp(-10)
+
+def func(x):
+    return math.exp(x) - (x+2)
+
+def main():
+    a = 0 
+    b = 2.0
+    while abs(b-a)>elps :
+        c = (a+b)/2
+        if func(a)*func(c)<0 :
+            b = c
+        else:
+            a = c
+    return c
+print(main())
+
+
